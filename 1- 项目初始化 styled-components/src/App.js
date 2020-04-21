d1d34bd0675a594c84a63dfa9be110e0ee20128e
ZY <13041895753@163.com>
2020-04-21 17:24:15 +0800

    ''

---
+++ 1- 项目初始化 styled-components/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div  className='dell'>
+      123
+    </div>
+  );
+}
+
+export default App;
---
+++ 1- 项目初始化 styled-components/src/index.js
@@ -0,0 +1,14 @@
+import React ,{Fragment} from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {GlobalStyle} from './style.js'
+
+
+ReactDOM.render(
+    <Fragment>
+        <GlobalStyle/>
+        <App/>
+    </Fragment>
+    ,document.getElementById('root'));
+
+
